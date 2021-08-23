21997046065c718d5ad582591ef0bfa9b3ff5c97
Noah Garner <noahdbgarner@gmail.com>
2021-08-22 22:45:08 -0700

    Add: links to sql and python tricks

--- sandbox.py
+++ sandbox.py
@@ -21,6 +21,9 @@ from structures.test_vars import *
 if __name__ == "__main__":
     """
     Contains leetcode algorithm testing
+    Python tips and tricks: https://holypython.com/100-python-tips-tricks/
+    SQL tips and tricks: https://coderpad.io/blog/sql-interview-questions/
+
     """
 
 
